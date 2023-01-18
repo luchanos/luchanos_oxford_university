d70dddc6d1396bfd1da85784fed42edb5be0fe88
luchanos <lukash_91@mail.ru>
2023-01-19 00:06:28 +0300

    refactor structure of project

---
+++ tests/test_handlers/test_create_handler.py
@@ -0,0 +1,74 @@
+import json
+
+import pytest
+
+
+async def test_create_user(client, get_user_from_database):
+    user_data = {
+      "name": "Nikolai",
+      "surname": "Sviridov",
+      "email": "lol@kek.com"
+    }
+    resp = client.post("/user/", data=json.dumps(user_data))
+    data_from_resp = resp.json()
+    assert resp.status_code == 200
+    assert data_from_resp["name"] == user_data["name"]
+    assert data_from_resp["surname"] == user_data["surname"]
+    assert data_from_resp["email"] == user_data["email"]
+    assert data_from_resp["is_active"] is True
+    users_from_db = await get_user_from_database(data_from_resp["user_id"])
+    assert len(users_from_db) == 1
+    user_from_db = dict(users_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert user_from_db["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] is True
+    assert str(user_from_db["user_id"]) == data_from_resp["user_id"]
+
+
+async def test_create_user_duplicate_email_error(client, get_user_from_database):
+    user_data = {
+      "name": "Nikolai",
+      "surname": "Sviridov",
+      "email": "lol@kek.com"
+    }
+    user_data_same = {
+      "name": "Petr",
+      "surname": "Petrov",
+      "email": "lol@kek.com"
+    }
+    resp = client.post("/user/", data=json.dumps(user_data))
+    data_from_resp = resp.json()
+    assert resp.status_code == 200
+    assert data_from_resp["name"] == user_data["name"]
+    assert data_from_resp["surname"] == user_data["surname"]
+    assert data_from_resp["email"] == user_data["email"]
+    assert data_from_resp["is_active"] is True
+    users_from_db = await get_user_from_database(data_from_resp["user_id"])
+    assert len(users_from_db) == 1
+    user_from_db = dict(users_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert user_from_db["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] is True
+    assert str(user_from_db["user_id"]) == data_from_resp["user_id"]
+    resp = client.post("/user/", data=json.dumps(user_data_same))
+    assert resp.status_code == 503
+    assert 'duplicate key value violates unique constraint "users_email_key"' in resp.json()["detail"]
+
+
+@pytest.mark.parametrize("user_data_for_creation, expected_status_code, expected_detail", [
+    ({}, 422, {'detail': [{'loc': ['body', 'name'], 'msg': 'field required', 'type': 'value_error.missing'},
+                          {'loc': ['body', 'surname'], 'msg': 'field required', 'type': 'value_error.missing'},
+                          {'loc': ['body', 'email'], 'msg': 'field required', 'type': 'value_error.missing'}]}),
+    ({"name": 123, "surname": 456, "email": "lol"}, 422, {'detail': 'Name should contains only letters'}),
+    ({"name": "Nikolai", "surname": 456, "email": "lol"}, 422, {'detail': 'Surname should contains only letters'}),
+    ({"name": "Nikolai", "surname": "Sviridov", "email": "lol"},
+     422, {'detail': [{'loc': ['body', 'email'],
+                       'msg': 'value is not a valid email address', 'type': 'value_error.email'}]}),
+])
+async def test_create_user_validation_error(client, user_data_for_creation, expected_status_code, expected_detail):
+    resp = client.post("/user/", data=json.dumps(user_data_for_creation))
+    data_from_resp = resp.json()
+    assert resp.status_code == expected_status_code
+    assert data_from_resp == expected_detail
