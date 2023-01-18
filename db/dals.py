42d50dd19f7e1ca6f6d51cbeb9696f109c7f7ade
luchanos <lukash_91@mail.ru>
2023-01-19 01:56:14 +0300

    tests

--- db/dals.py
+++ db/dals.py
@@ -1,7 +1,9 @@
 from typing import Union
 from uuid import UUID
 
-from sqlalchemy import update, and_, select
+from sqlalchemy import and_
+from sqlalchemy import select
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import User
@@ -13,12 +15,11 @@ from db.models import User
 
 class UserDAL:
     """Data Access Layer for operating user info"""
+
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    async def create_user(
-        self, name: str, surname: str, email: str
-    ) -> User:
+    async def create_user(self, name: str, surname: str, email: str) -> User:
         new_user = User(
             name=name,
             surname=surname,
@@ -29,9 +30,12 @@ class UserDAL:
         return new_user
 
     async def delete_user(self, user_id: UUID) -> Union[UUID, None]:
-        query = update(User).\
-            where(and_(User.user_id == user_id, User.is_active == True)).\
-            values(is_active=False).returning(User.user_id)
+        query = (
+            update(User)
+            .where(and_(User.user_id == user_id, User.is_active == True))
+            .values(is_active=False)
+            .returning(User.user_id)
+        )
         res = await self.db_session.execute(query)
         deleted_user_id_row = res.fetchone()
         if deleted_user_id_row is not None:
@@ -45,10 +49,12 @@ class UserDAL:
             return user_row[0]
 
     async def update_user(self, user_id: UUID, **kwargs) -> Union[UUID, None]:
-        query = update(User). \
-            where(and_(User.user_id == user_id, User.is_active == True)). \
-            values(kwargs). \
-            returning(User.user_id)
+        query = (
+            update(User)
+            .where(and_(User.user_id == user_id, User.is_active == True))
+            .values(kwargs)
+            .returning(User.user_id)
+        )
         res = await self.db_session.execute(query)
         update_user_id_row = res.fetchone()
         if update_user_id_row is not None:
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,18 @@
-from typing import Generator, Any
+import asyncio
+import os
+from typing import Any
+from typing import Generator
 
+import asyncpg
 import pytest
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 from starlette.testclient import TestClient
+
 import settings
-from main import app
-import os
-import asyncio
 from db.session import get_db
-import asyncpg
+from main import app
 
 
 CLEAN_TABLES = [
@@ -50,13 +53,18 @@ async def clean_tables(async_session_test):
 async def _get_test_db():
     try:
         # create async engine for interaction with database
-        test_engine = create_async_engine(settings.TEST_DATABASE_URL, future=True, echo=True)
+        test_engine = create_async_engine(
+            settings.TEST_DATABASE_URL, future=True, echo=True
+        )
 
         # create session for the interaction with database
-        test_async_session = sessionmaker(test_engine, expire_on_commit=False, class_=AsyncSession)
+        test_async_session = sessionmaker(
+            test_engine, expire_on_commit=False, class_=AsyncSession
+        )
         yield test_async_session()
     finally:
         pass
+
 
 @pytest.fixture(scope="function")
 async def client() -> Generator[TestClient, Any, None]:
@@ -72,26 +80,37 @@ async def client() -> Generator[TestClient, Any, None]:
 
 @pytest.fixture(scope="session")
 async def asyncpg_pool():
-    pool = await asyncpg.create_pool("".join(settings.TEST_DATABASE_URL.split("+asyncpg")))
+    pool = await asyncpg.create_pool(
+        "".join(settings.TEST_DATABASE_URL.split("+asyncpg"))
+    )
     yield pool
     pool.close()
 
 
 @pytest.fixture
 async def get_user_from_database(asyncpg_pool):
-
     async def get_user_from_database_by_uuid(user_id: str):
         async with asyncpg_pool.acquire() as connection:
-            return await connection.fetch("""SELECT * FROM users WHERE user_id = $1;""", user_id)
+            return await connection.fetch(
+                """SELECT * FROM users WHERE user_id = $1;""", user_id
+            )
 
     return get_user_from_database_by_uuid
 
 
 @pytest.fixture
 async def create_user_in_database(asyncpg_pool):
-
-    async def create_user_in_database(user_id: str, name: str, surname: str, email: str, is_active: bool):
+    async def create_user_in_database(
+        user_id: str, name: str, surname: str, email: str, is_active: bool
+    ):
         async with asyncpg_pool.acquire() as connection:
-            return await connection.execute("""INSERT INTO users VALUES ($1, $2, $3, $4, $5)""",
-                                            user_id, name, surname, email, is_active)
+            return await connection.execute(
+                """INSERT INTO users VALUES ($1, $2, $3, $4, $5)""",
+                user_id,
+                name,
+                surname,
+                email,
+                is_active,
+            )
+
     return create_user_in_database
