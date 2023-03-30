8b2ded74b87d1ad7399a3f0643485b65b47d00cd
luchanos <lukash_91@mail.ru>
2023-03-31 00:32:49 +0300

    [upd] root path

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.routing import APIRouter
 import settings
 from api.handlers import user_router
 from api.login_handler import login_router
+from api.service import service_router
 
 #########################
 # BLOCK WITH API ROUTES #
@@ -19,6 +20,7 @@ main_api_router = APIRouter()
 # set routes to the app instance
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 main_api_router.include_router(login_router, prefix="/login", tags=["login"])
+main_api_router.include_router(service_router, tags=["service"])
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
