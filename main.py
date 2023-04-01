43266e97f33b232c533ffbf3224eea896c96c792
luchanos <lukash_91@mail.ru>
2023-04-01 16:42:57 +0300

    [upd] root path

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
+from starlette_prometheus import metrics
+from starlette_prometheus import PrometheusMiddleware
 
 import settings
 from api.handlers import user_router
 from api.login_handler import login_router
 from api.service import service_router
+
+
+# sentry configuration
+sentry_sdk.init(
+    dsn=settings.SENTRY_URL,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
 
 #########################
 # BLOCK WITH API ROUTES #
@@ -16,6 +29,9 @@ app = FastAPI(title="luchanos-oxford-university")
 
 # create the instance for the routes
 main_api_router = APIRouter()
+
+app.add_middleware(PrometheusMiddleware)
+app.add_route("/metrics", metrics)
 
 # set routes to the app instance
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
